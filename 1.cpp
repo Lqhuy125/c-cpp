bf4b549615e43e9d88ee2b17b8df1063a6007950
Lqhuy <lequanghuy1252206@gmail.com>
2023-06-30 00:13:53 +0700

    oke

--- 1.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    float x1, y1, x2, y2, d;
-	cin>>x1>>y1>>x2>>y2;
-    d = sqrt((x2 - x1)*(x2 - x1) + (y2- y1)*(y2 - y1));
-    printf("%0.2f", d);
-}
